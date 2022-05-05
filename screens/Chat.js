1b0eae055eeb6c3efe3f57ec0c4e5a2e2bb58b54
Bingjing-Dong <dbingjing@gmail.com>
2022-05-05 03:48:19 -0400

    Setting up firebase

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,12 +1,16 @@
+/** @format */
+
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 function Chat() {
   return (
-    <View style={globalStyles.container}>
-      <Text>Chat Page</Text>
-    </View>
+    <GiftedChat />
+    // <View style={globalStyles.container}>
+    //   <Text>Chat Page</Text>
+    // </View>
   );
 }
 
