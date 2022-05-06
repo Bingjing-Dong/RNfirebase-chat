ea7724a53c501a68a953965c0892a02e01b42dc2
Bingjing-Dong <dbingjing@gmail.com>
2022-05-06 01:07:39 -0400

    set up signup page

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,14 +1,87 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { globalStyles } from '../styles/global';
+/** @format */
 
-function Signup({ navigation }) {
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../config/firebase';
+
+export default function Signup({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onHandleSignup = () => {
+    if (email !== '' && password !== '') {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(() => console.log('Signup success'))
+        .then(() => navigation.navigate('Chat'))
+        .catch((err) => {
+          Alert.alert(
+            'Alert Message',
+            'The email address already been used, please login!',
+            [
+              {
+                text: 'OK',
+                onPress: () => navigation.navigate('Login'),
+              },
+            ]
+          );
+          console.log(`Login err: ${err}`);
+        });
+    }
+  };
+
   return (
-    <View style={globalStyles.container}>
-      <Text>Signup Page</Text>
-      <Button title='Go to Chat' onPress={() => navigation.navigate('Chat')} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Create new account</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter email"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        textContentType="emailAddress"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter password"
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry={true}
+        textContentType="password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Button onPress={onHandleSignup} color="#f57c00" title="Signup" />
+      <Button
+        onPress={() => navigation.navigate('Login')}
+        title="Go to Login"
+      />
     </View>
   );
 }
 
-export default Signup;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#444',
+    alignSelf: 'center',
+    paddingBottom: 24,
+  },
+  input: {
+    backgroundColor: '#fff',
+    marginBottom: 20,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#333',
+    borderRadius: 8,
+    padding: 12,
+  },
+});
